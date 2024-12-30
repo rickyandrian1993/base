35d846cd374c7ef59ef9ba790c0523fe349dfaf4
Ricky Andrian <ricky.andrian@primecash.co.id>
2024-12-30 10:45:35 +0700

    Initial base

---
+++ electron.vite.config.mjs
@@ -0,0 +1,37 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    build: {
+      rollupOptions: {
+        /**
+         * Ignore "use client" waning since we are not using SSR
+         */
+        onwarn(warning, warn) {
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes('use client')) {
+            return
+          }
+          warn(warning)
+        }
+      }
+    },
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 8080
+    }
+  }
+})
